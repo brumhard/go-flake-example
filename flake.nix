9e309c6618b23a62d94fdc212efd3308092ae60f
Tobias Brumhard <tobias.brumhard@mail.schwarz>
2022-03-28 10:02:13 +0000

    Refine flake.nix

--- flake.nix
+++ flake.nix
@@ -10,32 +10,32 @@
     pkgs = nixpkgs.legacyPackages.${system};
   in
   rec {
-    packages = flake-utils.lib.flattenTree {
-      awesome-thing =
-      let
-        lib = pkgs.lib;
-      in
-      pkgs.buildGoModule {
-        pname = "awesome-thing";
-        version = "0.0.1";
+    packages.awesome-thing = pkgs.buildGoModule {
+      pname = "awesome-thing";
+      version = "0.0.1";
 
-        vendorSha256 = "sha256-QOeE2K/DydA+xbK3eebeCIzr2LcwNmXlDLs+oNzqmcM=";
-        subPackages = [ "cmd/awesome-thing" ];
-        src = ./.;
+      vendorSha256 = "sha256-QOeE2K/DydA+xbK3eebeCIzr2LcwNmXlDLs+oNzqmcM=";
+      subPackages = [ "cmd/awesome-thing" ];
+      src = ./.;
 
-        meta = {
-          description = "Awesome Project";
-          license = lib.licenses.mit;
-          maintainers = [ "brumhard" ];
-          platforms = lib.platforms.linux ++ lib.platforms.darwin;
-        };
+      meta = {
+        description = "Awesome Project";
+        license = pkgs.lib.licenses.mit;
+        maintainers = [ "brumhard" ];
+        platforms = pkgs.lib.platforms.linux ++ pkgs.lib.platforms.darwin;
       };
     };
 
+    # to display `nix flake show .` correctly if `defaultApp` is set
+    # see https://github.com/NixOS/nix/pull/5652
+    apps.awesome-thing = {
+      type = "app";
+      program = "${self.packages.${system}.awesome-thing}/bin/awesome-thing";
+    };
+
     defaultPackage = packages.awesome-thing;
-    defaultApp = packages.awesome-thing;
+    defaultApp = apps.awesome-thing;
 
     devShell = import ./shell.nix { inherit pkgs; };
-  }
-  );
+  });
 }
